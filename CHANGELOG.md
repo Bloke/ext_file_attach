7ea56e635ced1ab1b9a7e4dddb4f61451a9568de
Stef Dawson <thatbloke@stefdawson.com>
2017-12-07 00:58:29 +0000

    Unbump version (it had already been done)
    Add CHANGELOG too.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 1.0.1 - 2017-12-07
+
+* Fix 'Multiple or malformed newlines' error during delivery.
+* Add composer/packagist furniture (thanks, philwareham).
+
+## 1.0.0 - 2017-07-19
+
+* Initial release.
--- ext_file_attach.php
+++ ext_file_attach.php
@@ -17,7 +17,7 @@ $plugin['name'] = 'ext_file_attach';
 // 1 = Plugin help is in raw HTML.  Not recommended.
 # $plugin['allow_html_help'] = 1;
 
-$plugin['version'] = '1.0.2';
+$plugin['version'] = '1.0.1';
 $plugin['author'] = 'Stef Dawson';
 $plugin['author_uri'] = 'https://stefdawson.com/';
 $plugin['description'] = 'Add file upload ability to com_connect';
